59b502427818d388f21dd4667cd8ebe7289d3af5
jaimegarciareyes <jaimegarciareyes@LEM231-16>
2023-01-18 18:56:12 -0800

    Add project files.

---
+++ COMP003A.Assignment1/Program.cs
@@ -0,0 +1,24 @@
+﻿// this is a single-line comment
+/* this is a multi-line comment 
+ * Author: Jaime Garcia Reyes
+ * Course: COMP-003A
+ * Purpose: lecture activity for this week
+ */
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me"); //add comments here too
+            Console.WriteLine("1. My name is Jaime Garcia");
+            Console.WriteLine("2. I have");
+            Console.WriteLine("3. ");
+            Console.WriteLine("4. ");
+            Console.WriteLine("5. ");
+            Console.WriteLine("6. ");
+            Console.WriteLine("7. ");
+            Console.WriteLine("8. ");
+        }
+    }
+}
